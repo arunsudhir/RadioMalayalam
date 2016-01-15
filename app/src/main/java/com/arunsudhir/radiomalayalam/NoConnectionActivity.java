ac91e4681efe33b6b2ea328d7101d42eb41ffa72
Arun Sudhir <arunsudhir@gmail.com>
2016-01-15 15:38:41 -0600

    Reload PlaylistActivity on clicking "Try Again"

--- app/src/main/java/com/arunsudhir/radiomalayalam/NoConnectionActivity.java
+++ app/src/main/java/com/arunsudhir/radiomalayalam/NoConnectionActivity.java
@@ -1,7 +1,12 @@
 package com.arunsudhir.radiomalayalam;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.arunsudhir.radiomalayalam.service.PlayerService;
 
 public class NoConnectionActivity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class NoConnectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_no_connection);
+
+        // Add click listener to the retry (Try again) button
+        final Button buttonTryAgain = (Button) findViewById(R.id.button_try_again);
+        buttonTryAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent serviceIntent = new Intent(thisActivity, PlayListActivity.class);
+                try {
+                        startService(serviceIntent);
+                } catch (Exception e) {
+                    //LOG.error(e, "Failed to toggle state");
+                }
+            }
+        });
     }
 }
