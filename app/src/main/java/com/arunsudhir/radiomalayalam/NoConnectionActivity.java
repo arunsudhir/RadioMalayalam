782fa254a874aeae557ec4989f96b8b084764684
Arun Sudhir <arunsudhir@gmail.com>
2016-01-16 01:28:03 -0800

    fix null ref in adcontrol on app load

--- app/src/main/java/com/arunsudhir/radiomalayalam/NoConnectionActivity.java
+++ app/src/main/java/com/arunsudhir/radiomalayalam/NoConnectionActivity.java
@@ -20,7 +20,7 @@ public class NoConnectionActivity extends AppCompatActivity {
         buttonTryAgain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent playListIntent = new Intent(noConnectionActivity, PlayListActivity.class);
+                Intent playListIntent = new Intent(noConnectionActivity,  PlayListActivity.class);
                 try {
                         startActivity(playListIntent);
                 } catch (Exception e) {
